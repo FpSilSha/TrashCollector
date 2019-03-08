df005e5fbf78e553e56a691704ef47d06c29d9b5
Fp38SilSha <faperez38s@gmail.com>
2019-03-08 11:08:09 -0600

    Customer Model created. Added first and last name, state, city, zip, street address
    Migrations enabled as well with a config file update.

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,14 @@ namespace TrashCollector.Models
 {
     public class Customer
     {
-
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+        public string Street_Address { get; set; }
 
     }
 }
