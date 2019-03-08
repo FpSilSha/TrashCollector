0eefe0e8ef4fa856973c49070226f93ed8a8cb21
Fp38SilSha <faperez38s@gmail.com>
2019-03-08 11:39:11 -0600

    Created a model for Employee, which also creates the table. Employees now give their First and Last names, their employee number, and their current working zip, which I'll make mutable later.

---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int WorkingZip { get; set; }
+        
+    }
+}
