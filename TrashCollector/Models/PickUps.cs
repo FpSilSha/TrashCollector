249bd32229b8b01503ad4794024b0ee3f66e0718
Fp38SilSha <faperez38s@gmail.com>
2019-03-08 11:52:50 -0600

    Added a PickUps model that has strings of PickUpDay and ExtraDay, DateTime for suspend start and end dates, and Decimal CurrentCharges.

---
+++ TrashCollector/Models/PickUps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class PickUps
+    {
+        public string PickUpDay { get; set; }
+        public string ExtraDay { get; set; }
+        
+        public DateTime? SuspendStart { get; set; }
+        public DateTime? SuspendEnd { get; set; }
+        public decimal CurrentCharges { get; set; }
+    }
+}
