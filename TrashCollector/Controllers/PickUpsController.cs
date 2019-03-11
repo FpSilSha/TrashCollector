1ce6e74ad56f8b315b1703249b5a5aecd4b3cd42
Fp38SilSha <faperez38s@gmail.com>
2019-03-11 18:45:17 -0500

    Logic for the creation of a pickup table thrown in. Gets the user's customer id to set up the FK relation. Then adds it to the information the customer provides in creation of the pickup object, then submits it to the database.

--- TrashCollector/Controllers/PickUpsController.cs
+++ TrashCollector/Controllers/PickUpsController.cs
@@ -8,41 +8,43 @@ using Microsoft.AspNet.Identity;
 
 namespace TrashCollector.Controllers
 {
-    public class CustomerController : Controller
+    public class PickUpsController : Controller
     {
         ApplicationDbContext db;
-        public CustomerController()
+        public PickUpsController()
         {
             db = new ApplicationDbContext();
         }
-        // GET: Customer
+        // GET: PickUps
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Customer/Details/5
+        // GET: PickUps/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Customer/Create
+        // GET: PickUps/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Customer/Create
+        // POST: PickUps/Create
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Create(PickUps pickUps)
         {
             try
             {
-                customer.ApplicationUserId = User.Identity.GetUserId();
-                db.Customers.Add(customer);
+                var user = User.Identity.GetUserId();
+                var customer = db.Customers.Where(c => c.ApplicationUserId == user).Single();
+                pickUps.CustomerId = customer.Id;
+                db.PickUps.Add(pickUps);
                 db.SaveChanges();
-                return RedirectToAction("Create", "PickUps");
+                return RedirectToAction("Index");
             }
             catch
             {
@@ -50,13 +52,13 @@ namespace TrashCollector.Controllers
             }
         }
 
-        // GET: Customer/Edit/5
+        // GET: PickUps/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Customer/Edit/5
+        // POST: PickUps/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
@@ -72,13 +74,13 @@ namespace TrashCollector.Controllers
             }
         }
 
-        // GET: Customer/Delete/5
+        // GET: PickUps/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Customer/Delete/5
+        // POST: PickUps/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
